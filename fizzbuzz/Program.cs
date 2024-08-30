174d6830dc1d69813daf1eb1ccb956c646d38c15
Gem Code <114959173+Dbrown127@users.noreply.github.com>
2024-08-29 22:21:04 -0400

    fizzbuzz: initial commits for fizz buzz dev

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var fizz = "Fizz";
+var buzz = "Buzz";
+var fb = fizz + buzz;
+
+//value represent the index 
+var index = 1;
+
+//Add a for loop to that loops to 100
+for (var i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{index++}. Let Play {fb}");
+}
