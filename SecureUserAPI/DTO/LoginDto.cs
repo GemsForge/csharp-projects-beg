e9b3d407668c1eeaaef682688e1b3d6def607eb3
Gem Code <114959173+Dbrown127@users.noreply.github.com>
2024-09-20 23:51:15 -0400

    su(feat): #26 created the dto objects for register, login and password reset

---
+++ SecureUserAPI/DTO/LoginDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecureUserAPI.DTO
+{
+    /// <summary>
+    /// Data Transfer Object for user login.
+    /// </summary>
+    public class LoginDto
+    {
+        /// <summary>
+        /// Gets or sets the email address of the user.
+        /// </summary>
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public required string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public required string Password { get; set; }
+    }
+}
---
+++ SecureUserAPI/DTO/PasswordResetDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecureUserAPI.DTO
+{
+    /// <summary>
+    /// Data Transfer Object for password reset.
+    /// </summary>
+    public class PasswordResetDto
+    {
+        /// <summary>
+        /// Gets or sets the email address of the user.
+        /// </summary>
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public required string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username should be between 2 and 50 characters.")]
+        public required string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name of the user.
+        /// </summary>
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name should be between 2 and 50 characters.")]
+        public required string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new password for the user.
+        /// </summary>
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "New password must be at least 8 characters long.")]
+        public required string NewPassword { get; set; }
+    }
+}
